67b6b95265f666a46099645315afe16dba5a17f3
Astrid88 <giu.munari@gmail.com>
2019-09-11 14:24:10 +0200

    add css

--- src/components/Game/GameContainer.js
+++ src/components/Game/GameContainer.js
@@ -7,6 +7,7 @@ import {
     getRandomWord,
     guessCharacter
 } from '../../actions/game'
+import Hangman from './Hangman';
 
 class GameContainer extends Component {
     state = { character: '' }
@@ -32,6 +33,8 @@ class GameContainer extends Component {
                 {(this.props.gameWord && this.props.status === 'open') &&
                     <div>
                         <h1>Guess the word</h1>
+                        <p>You have {this.props.points/2} temptatives</p>
+                        <Hangman points={this.props.points}/>
                         <Word word={this.props.displayWord} />
                         <Form
                             onSubmit={this.onSubmit}
@@ -44,9 +47,10 @@ class GameContainer extends Component {
                 {(this.props.status && this.props.status !== 'open') &&
                     <div>
                         <h1>{this.props.status}</h1>
+                        <Hangman points={this.props.points}/>
                         <h2>Your score: {this.props.points}</h2>
                         <h2>{this.props.gameWord}</h2>
-                        <button onClick={this.componentDidMount}>Start a New Game</button>
+                        <button onClick={this.props.getRandomWord} className='button'>Start a New Game</button>
                     </div>
                 }
             </div>
